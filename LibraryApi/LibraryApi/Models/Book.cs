cdc78c200bc4adbeb2cc4eb1693d9f208a564ed1
Jules Esnault <julesnault@gmail.com>
2021-01-27 10:22:35 +0100

    api livres

---
+++ LibraryApi/LibraryApi/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models
+{
+    [DataContract(Name = "book")]
+    public class Book
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+        [DataMember(Name = "author")]
+        public string Author { get; set; }
+        [DataMember(Name = "publicationDate")]
+        public DateTime PublicationDate { get; set; }
+    }
+}
---
+++ LibraryApi/LibraryApi/Services/LibraryApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LibraryApi.Models;
+
+namespace LibraryApi.Services
+{
+    public class LibraryApiContext : DbContext
+    {
+        public LibraryApiContext (DbContextOptions<LibraryApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LibraryApi.Models.Book> Book { get; set; }
+
+    }
+}
